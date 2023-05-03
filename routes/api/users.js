62aa0cc54f00f8b56b55bc9c6b55b31d489aa928
Tom Leger <tomleger@gmail.com>
2023-05-03 16:28:29 -0400

    working registration/login

--- routes/api/users.js
+++ routes/api/users.js
@@ -33,4 +33,26 @@ router.post('/register', (req, res) => {
     })
 })
 
+//A route to allow users to login 
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+    .then(user => {
+        if(!user) {
+            return res.status(404).json({email: 'This user does not exist'})
+        }
+
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if(isMatch) {
+                res.json({msg: 'Succcess'});
+            } else {
+                return res.status(400).json({password: 'Incorrect password'});
+            }
+        })
+    })
+})
+
 module.exports = router;
